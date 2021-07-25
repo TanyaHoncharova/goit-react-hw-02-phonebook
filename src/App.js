2b5c69d2e12ffb828adf311d2bb454ee61c1fa36
TanyaHoncharova <tanushaGK@gmail.com>
2021-07-25 13:17:35 +0300

    adding recurring contacts fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import shortid from 'shortid';
 import './App.css';
 
@@ -7,6 +8,23 @@ import ContactList from './Components/ContactList';
 import Filter from './Components/Filter';
 
 class App extends Component {
+    static defaultProps = {
+    totalContactsCount: null,
+    visibleContacts: null,
+  }
+  static propTypes = {
+    contacts: PropTypes.arrayOf(
+      PropTypes.shape(
+        {
+          id: PropTypes.any.isRequired,
+          name: PropTypes.string.isRequired,
+          number: PropTypes.string.isRequired,
+        })
+    ),
+    filter: PropTypes.string,
+    totalContactsCount: PropTypes.number,
+    visibleContacts: PropTypes.number,
+  };
   state = {
     contacts: [
       {id: 'id-1', name: 'Margo Robins', number: '093-144-15-14'},
@@ -18,11 +36,11 @@ class App extends Component {
   };
 
   addContact = (name, number) => {
-    const normolaseName = name.toLowerCase();
-    const isInContacts = this.state.contacts.some(contact => contact.name === normolaseName)
-    if (isInContacts) { alert(`${name} is already in contacts.`); return }
-    
-    if (name && number) {
+ 
+    if (this.state.contacts.find((contact) => name.toLowerCase() === contact.name.toLocaleLowerCase()))
+    {
+      alert(`${name} is already in contacts.`); return;
+    } else if (name && number) {
       const contact = {
         id: shortid.generate(),
         name,
@@ -31,6 +49,8 @@ class App extends Component {
       this.setState(prevState => ({
         contacts: [contact, ...prevState.contacts],
       }));
+    } else {
+      alert (`Please enter a name for contact ${name}!`)
     }
   };
 
@@ -49,7 +69,7 @@ class App extends Component {
     const totalContactsCount = contacts.length;
 
     const normalizedFilter = filter.toLowerCase();
-    const visibleContacts = contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter),);
+    const visibleContacts = contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
 
     return (
       <div className="App">
